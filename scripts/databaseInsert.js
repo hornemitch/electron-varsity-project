4c6f3b7de6396ec07c252563c0c1271fbbc6fc13
hornemitch <mitchhorne92@gmail.com>
2016-05-05 09:46:26 +0200

    Created enter functions

---
+++ scripts/databaseInsert.js
@@ -0,0 +1,67 @@
+function enterClinic(){
+
+}
+
+function enterPen(){
+
+}
+
+function enterStaff(){
+
+}
+
+function enterOwner(){
+
+}
+
+function enterPet(){
+
+}
+
+function enterAppoint(){
+
+}
+
+function enterExam(){
+
+}
+
+function enterPetStay(){
+
+}
+
+function enterPetTreat(){
+
+}
+
+function enterStayTreat(){
+
+}
+
+function enterInv(){
+
+}
+
+function enterInvPay(){
+
+}
+
+function enterNonSurg(){
+
+}
+
+function enterSurg(){
+
+}
+
+function enterPhar(){
+
+}
+
+function enterTreat(){
+
+}
+
+function enterTreatCost(){
+
+}
